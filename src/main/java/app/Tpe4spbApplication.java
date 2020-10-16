81757e7cbee9ed423b594ddebe44591ef827a98f
IgnacioFondovila <70292476+IgnacioFondovila@users.noreply.github.com>
2020-10-16 20:37:30 -0300

    Initial commit

---
+++ src/main/java/app/Tpe4spbApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpe4spbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpe4spbApplication.class, args);
+    }
+
+}
